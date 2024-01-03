772038f733700fe2708dce90d23e6d44e1fb8517
chenfan <1042389894@qq.com>
2024-01-03 15:28:15 +0800

    :style 补充Swagger注解

--- src/main/java/com/dsj/csp/manage/controller/SupportController.java
+++ src/main/java/com/dsj/csp/manage/controller/SupportController.java
@@ -4,15 +4,14 @@ import com.baomidou.mybatisplus.extension.plugins.pagination.Page;
 import com.dsj.common.dto.Result;
 import com.dsj.csp.manage.converter.SupportConverter;
 import com.dsj.csp.manage.dto.SupportDto;
-import com.dsj.csp.manage.dto.request.SupportAcceptRequest;
-import com.dsj.csp.manage.dto.request.SupportQueryRequest;
-import com.dsj.csp.manage.dto.request.SupportReplyRequest;
-import com.dsj.csp.manage.dto.request.SupportUpdateRequest;
+import com.dsj.csp.manage.dto.request.*;
 import com.dsj.csp.manage.dto.response.SupportCommunicationHistoryResponse;
 import com.dsj.csp.manage.dto.response.SupportQueryResponse;
 import com.dsj.csp.manage.entity.SupportEntity;
 import com.dsj.csp.manage.service.SupportService;
 import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.Parameter;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 
@@ -23,8 +22,9 @@ import java.util.List;
  * @version 1.0.0
  * @since 2023/12/26 15:44
  */
+@Tag(name = "工单管理")
 @RestController
-@RequestMapping("/system/support")
+@RequestMapping("/support")
 public class SupportController {
     @Autowired
     private SupportService supportService;
@@ -45,13 +45,13 @@ public class SupportController {
 
     @Operation(summary = "获取工单详情")
     @GetMapping(value = "/{supportId}")
-    public Result<SupportDto> getInfo(@PathVariable("supportId") Long supportId) {
+    public Result<SupportDto> getInfo(@Parameter(description = "工单ID") @PathVariable("supportId") Long supportId) {
         return Result.success(SupportConverter.toSupportDto(supportService.selectSupportById(supportId)));
     }
 
     @Operation(summary = "受理工单")
     @PostMapping(value = "/{supportId}/accept")
-    public Result<SupportDto> accept(@PathVariable Long supportId, @RequestBody SupportAcceptRequest request) {
+    public Result<SupportDto> accept(@Parameter(description = "工单ID") @PathVariable Long supportId, @RequestBody SupportAcceptRequest request) {
         return Result.success(SupportConverter.toSupportDto(supportService.acceptSupport(supportId, request)));
     }
 
@@ -63,20 +63,20 @@ public class SupportController {
 
     @Operation(summary = "更新工单")
     @PutMapping(value = "/{supportId}")
-    public Result<SupportDto> update(@PathVariable Long supportId, @RequestBody SupportUpdateRequest request) {
+    public Result<SupportDto> update(@Parameter(description = "工单ID") @PathVariable Long supportId, @RequestBody SupportUpdateRequest request) {
         return Result.success("更新成功", SupportConverter.toSupportDto(supportService.updateSupport(supportId, request)));
     }
 
     @Operation(summary = "删除工单")
     @DeleteMapping("/{supportId}")
-    public Result<Void> remove(@PathVariable Long supportId) {
+    public Result<Void> remove(@Parameter(description = "工单ID") @PathVariable Long supportId) {
         supportService.deleteSupportById(supportId);
         return Result.success("删除成功", null);
     }
 
     @Operation(summary = "完成工单")
     @PostMapping("/{supportId}/finish")
-    public Result<SupportDto> supportFinish(@PathVariable Long supportId) {
+    public Result<SupportDto> supportFinish(@Parameter(description = "工单ID") @PathVariable Long supportId) {
         return Result.success(SupportConverter.toSupportDto(supportService.supportFinish(supportId)));
     }
 

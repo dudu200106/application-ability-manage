2f88d9cf12068140727fdc0532db0c600c7e7bdb
chenfan <1042389894@qq.com>
2024-01-03 16:25:07 +0800

    :style 包名改为小写

--- src/main/java/com/dsj/csp/common/config/mybatisplus/MybatisMetaObjectHandler.java
+++ src/main/java/com/dsj/csp/common/config/mybatisplus/MybatisMetaObjectHandler.java
@@ -1,10 +1,9 @@
-package com.dsj.csp.common.config.mybatisPlus;
+package com.dsj.csp.common.config.mybatisplus;
 
 import com.baomidou.mybatisplus.core.handlers.MetaObjectHandler;
 import org.apache.ibatis.reflection.MetaObject;
 import org.springframework.stereotype.Component;
 
-import java.time.LocalDateTime;
 import java.util.Date;
 
 /**

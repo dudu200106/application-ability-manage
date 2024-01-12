5452e4f0152147aaeaba5ac5754a259271b7c6b5
yuanqiyan <yqy423510793@163.com>
2024-01-12 09:38:27 +0800

    更新了文件名，不影响程序

--- src/main/java/com/dsj/csp/manage/controller/UserApproveController.java
+++ src/main/java/com/dsj/csp/manage/controller/UserApproveController.java
@@ -9,7 +9,7 @@ import org.springframework.web.multipart.MultipartFile;
 
 @RestController
 @RequestMapping("/user")
-public class UserController {
+public class UserApproveController {
     @Autowired
     private UserApproveService userApproveService;
 

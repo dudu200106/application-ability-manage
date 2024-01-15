3e33cdbce3f995a4cdda27e0803c33cad279b028
杜顺昌 <shunchangdu@sina.com>
2024-01-15 16:33:38 +0800

    替换 ManageApplicationMapper.java

--- src/main/java/com/dsj/csp/manage/mapper/ManageApplicationMapper.java
+++ src/main/java/com/dsj/csp/manage/mapper/ManageApplicationMapper.java
@@ -6,18 +6,24 @@ import com.baomidou.mybatisplus.core.mapper.BaseMapper;
 import java.util.List;
 
 /**
-* @author DSCBooK
-* @description 针对表【MANAGE_APPLICATION(应用列表)】的数据库操作Mapper
-* @createDate 2024-01-11 10:43:10
-* @Entity generator.entity.ManageApplication
-*/
+ * @author DSCBooK
+ * @description 针对表【MANAGE_APPLICATION(应用列表)】的数据库操作Mapper
+ * @createDate 2024-01-11 10:43:10
+ * @Entity generator.entity.ManageApplication
+ */
 public interface ManageApplicationMapper extends BaseMapper<ManageApplication> {
 
-    List<ManageApplication> selectappID(Long appId , String appUserId );
+    List<ManageApplication> selectappID(Long appId, String appUserId);
+
     int contAll();
-    List<ManageApplication>selectUserApp(String userId );
-    boolean updateSecret(Long appId,String appKey,String appSecret);
-    boolean upadataAppList(Long appId,String appName,String appSynopsis,String appIconpath,String appUserId);
+
+    List<ManageApplication> selectUserApp(String appUserId);
+
+    boolean updateSecret(Long appId, String appKey, String appSecret);
+
+    boolean upadataAppList(Long appId, String appName, String appSynopsis, String appIconpath, String appUserId);
+
+    boolean updateIsdetele(Long appId, String appUserId);
 
 }
 

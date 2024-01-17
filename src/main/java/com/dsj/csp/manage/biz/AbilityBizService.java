422e5eeeff9d8919e472f0655c02d5a8253295a1
caiyun <616898549@qq.com>
2024-01-17 14:12:01 +0800

    feat:引入biz层，示例

---
+++ src/main/java/com/dsj/csp/manage/biz/AbilityBizService.java
@@ -0,0 +1,14 @@
+package com.dsj.csp.manage.biz;
+
+import com.dsj.csp.manage.dto.AbilityLoginVO;
+
+/**
+ * 功能说明：
+ *
+ * @author 蔡云
+ * 2024/1/17
+ */
+public interface AbilityBizService {
+
+    Boolean saveAbility(AbilityLoginVO abilityLoginVO);
+}

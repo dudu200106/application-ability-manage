ea99e178dfec49577df47095de1172a038d56fff
SeanDu <1247187273@qq.com>
2024-01-21 15:25:19 +0800

    fix: 修改能力使用申请审核逻辑代码

--- src/main/java/com/dsj/csp/manage/biz/impl/AbilityApplyBizServiceImpl.java
+++ src/main/java/com/dsj/csp/manage/biz/impl/AbilityApplyBizServiceImpl.java
@@ -1,6 +1,7 @@
 package com.dsj.csp.manage.biz.impl;
 
 import cn.hutool.core.date.DateTime;
+import com.baomidou.mybatisplus.core.conditions.query.LambdaQueryWrapper;
 import com.baomidou.mybatisplus.core.conditions.update.LambdaUpdateWrapper;
 import com.baomidou.mybatisplus.core.toolkit.Wrappers;
 import com.dsj.csp.manage.biz.AbilityApplyBizService;
@@ -14,12 +15,15 @@ import com.dsj.csp.manage.service.AbilityApplyService;
 import com.dsj.csp.manage.service.AbilityService;
 import com.dsj.csp.manage.service.ManageApplicationService;
 import com.dsj.csp.manage.service.UserApproveService;
+import com.dsj.csp.manage.util.Sm2;
 import com.dsj.csp.manage.util.Sm4;
 import lombok.RequiredArgsConstructor;
 import org.springframework.beans.BeanUtils;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Propagation;
 import org.springframework.transaction.annotation.Transactional;
+
+import java.util.Map;
 
 
 @RequiredArgsConstructor
@@ -62,27 +66,33 @@ public class AbilityApplyBizServiceImpl implements AbilityApplyBizService {
         updateWrapper.set(AbilityApplyEntity::getUpdateTime, DateTime.now());
         abilityApplyService.update(updateWrapper);
 
-        // 判断是否生成APP Key 和 Secret Key
-        ManageApplicationEntity app = manageApplicationService.getById(auditVO.getAppId());
-//        Long appId = abilityApplyService.getOne()
+        // 判断是否要生成一对密钥
+        LambdaQueryWrapper<AbilityApplyEntity> applyQW
+                = Wrappers.lambdaQuery(AbilityApplyEntity.class)
+                .select(AbilityApplyEntity::getAppId)
+                .eq(AbilityApplyEntity::getAbilityApplyId, auditVO.getAbilityApplyId());
+        Long appId = abilityApplyService.getOne(applyQW).getAppId();
+        ManageApplicationEntity app = manageApplicationService.getById(appId);
+        // 如果申请的appId不存在
         if (app == null){
             return;
         }
         String appSecretKey =  app.getAppSecret();
         String appAppKey =  app.getAppSecret();
+        // 如果审核结果不通过 或者应用的公钥私钥有一个不为空, 就不用生成密钥了
         if (auditVO.getFlag() != 1
                 || (appSecretKey!=null && !"".equals(appSecretKey))
                 || (appAppKey!=null && !"".equals(appAppKey))){
             return;
         }
-        String appKey = Sm4.sm();
-        String secretKey = Sm4.sm();
-        LambdaUpdateWrapper<ManageApplicationEntity> appUpdateWrapper = Wrappers.lambdaUpdate();
-        // 设置更新条件，这里假设要更新 id 为 1 的记录
-        appUpdateWrapper.eq(ManageApplicationEntity::getAppId, auditVO.getAppId());
-        // 设置要更新的字段和值
-        appUpdateWrapper.set(ManageApplicationEntity::getAppKey, appKey);
-        appUpdateWrapper.set(ManageApplicationEntity::getAppSecret, secretKey);
+        Map<String, String> sm2Map = Sm2.sm2Test();
+        String appKey = sm2Map.get("publicEncode");
+        String secretKey = sm2Map.get("privateEncode");
+        LambdaUpdateWrapper<ManageApplicationEntity> appUpdateWrapper
+                = Wrappers.lambdaUpdate(ManageApplicationEntity.class)
+                .eq(ManageApplicationEntity::getAppId, appId)
+                .set(ManageApplicationEntity::getAppKey, appKey)
+                .set(ManageApplicationEntity::getAppSecret, secretKey);
         manageApplicationService.update(appUpdateWrapper);
     }
 }

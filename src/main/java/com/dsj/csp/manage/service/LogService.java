9ac95779ceb83449d9c665e1a718513112ba8013
fanbaoliang <1131052699@qq.com>
2024-02-01 17:07:21 +0800

    日志列表查询删除

---
+++ src/main/java/com/dsj/csp/manage/service/LogService.java
@@ -0,0 +1,16 @@
+package com.dsj.csp.manage.service;
+
+import com.baomidou.mybatisplus.extension.plugins.pagination.Page;
+import com.baomidou.mybatisplus.extension.service.IService;
+import com.dsj.csp.manage.dto.ManageApplictionVo;
+import com.dsj.csp.manage.entity.LogEntity;
+import com.dsj.csp.manage.entity.UserApproveEntity;
+import com.github.yulichang.wrapper.MPJLambdaWrapper;
+
+import java.util.Date;
+
+public interface LogService extends IService<LogEntity> {
+    void removeAll();
+
+    Page<LogEntity> select(String keyword, Date startTime, Date endTime, int page, int size);
+}
---
+++ src/main/java/com/dsj/csp/manage/service/impl/LogServiceImpl.java
@@ -0,0 +1,40 @@
+package com.dsj.csp.manage.service.impl;
+
+import com.baomidou.mybatisplus.core.conditions.query.QueryWrapper;
+import com.baomidou.mybatisplus.extension.plugins.pagination.Page;
+import com.baomidou.mybatisplus.extension.service.impl.ServiceImpl;
+import com.dsj.csp.manage.entity.LogEntity;
+import com.dsj.csp.manage.mapper.LogMapper;
+import com.dsj.csp.manage.service.LogService;
+import jakarta.annotation.Resource;
+import org.apache.commons.lang3.StringUtils;
+import org.springframework.stereotype.Service;
+
+import java.util.Date;
+import java.util.Objects;
+
+@Service
+public class LogServiceImpl extends ServiceImpl<LogMapper, LogEntity> implements LogService {
+
+    @Resource
+    private LogMapper logMapper;
+    @Override
+    public void removeAll() {
+        logMapper.removeAll();
+    }
+
+    @Override
+    public Page<LogEntity> select(String keyword, Date startTime, Date endTime, int page, int size) {
+        QueryWrapper<LogEntity> wrapper = new QueryWrapper();
+        wrapper.lambda()
+                .between(Objects.nonNull(startTime) && Objects.nonNull(endTime), LogEntity::getCreateTime, startTime, endTime)
+                .and(StringUtils.isNotBlank(keyword), lambdaQuery->{
+                    lambdaQuery
+                            .like(StringUtils.isNotBlank(keyword), LogEntity::getLogContent, keyword)
+                            .or()
+                            .like(StringUtils.isNotBlank(keyword), LogEntity::getUsername, keyword);
+                });
+        return baseMapper.selectPage(new Page(page, size), wrapper);
+
+    }
+}

a260e66e4301b30a489f47fafd4b8ac4356cd505
dushunchang <shunchangdu@sina.com>
2024-02-04 14:39:30 +0800

    添加日志切面获取客户端IP

--- src/main/java/com/dsj/csp/manage/util/IP.java
+++ src/main/java/com/dsj/csp/manage/util/IP.java
@@ -30,25 +30,78 @@ public class IP {
      * @param request 请求
      * @return ip地址
      */
-    public static String getIpAddress(HttpServletRequest request) {
-        String ip = request.getHeader("X-Real-IP");
-        if (!StringUtils.isBlank(ip) && !"unknown".equalsIgnoreCase(ip)) {
-            return ip;
-        }
-        ip = request.getHeader("X-Forwarded-For");
-        if (!StringUtils.isBlank(ip) && !"unknown".equalsIgnoreCase(ip)) {
-            // 多次反向代理后会有多个IP值，第一个为真实IP。
-            int index = ip.indexOf(',');
-            if (index != -1) {
-                return ip.substring(0, index);
-            } else {
-                return ip;
+    public static String getIpAddress(HttpServletRequest request){
+        String ipAddress = null;
+        try {
+            ipAddress = request.getHeader("x-forwarded-for");
+            if (ipAddress == null || ipAddress.length() == 0 || "unknown".equalsIgnoreCase(ipAddress)) {
+                ipAddress = request.getHeader("Proxy-Client-IP");
             }
-        } else {
-            return request.getRemoteAddr();
+            if (ipAddress == null || ipAddress.length() == 0 || "unknown".equalsIgnoreCase(ipAddress)) {
+                ipAddress = request.getHeader("WL-Proxy-Client-IP");
+            }
+            if (ipAddress == null || ipAddress.length() == 0 || "unknown".equalsIgnoreCase(ipAddress)) {
+                ipAddress = request.getRemoteAddr();
+                if (ipAddress.equals("127.0.0.1")) {
+                    // 根据网卡取本机配置的IP
+                    InetAddress inet = null;
+                    try {
+                        inet = InetAddress.getLocalHost();
+                    } catch (UnknownHostException e) {
+                        e.printStackTrace();
+                    }
+                    ipAddress = inet.getHostAddress();
+                }
+            }
+            // 对于通过多个代理的情况，第一个IP为客户端真实IP,多个IP按照','分割
+            if (ipAddress != null && ipAddress.length() > 15) { // "***.***.***.***".length()
+                // = 15
+                if (ipAddress.indexOf(",") > 0) {
+                    ipAddress = ipAddress.substring(0, ipAddress.indexOf(","));
+                }
+            }
+        } catch (Exception e) {
+            ipAddress="";
         }
+        // ipAddress = this.getRequest().getRemoteAddr();
 
+        return ipAddress;
     }
+//    {
+//        String ipAddress = null;
+//        try {
+//            ipAddress = request.getHeader("x-forwarded-for");
+//            if (ipAddress == null || ipAddress.length() == 0 || "unknown".equalsIgnoreCase(ipAddress)) {
+//                ipAddress = request.getHeader("Proxy-Client-IP");
+//            }
+//            if (ipAddress == null || ipAddress.length() == 0 || "unknown".equalsIgnoreCase(ipAddress)) {
+//                ipAddress = request.getHeader("WL-Proxy-Client-IP");
+//            }
+//            if (ipAddress == null || ipAddress.length() == 0 || "unknown".equalsIgnoreCase(ipAddress)) {
+//                ipAddress = request.getRemoteAddr();
+//                if ("127.0.0.1".equals(ipAddress)) {
+//                    // 根据网卡取本机配置的IP
+//                    InetAddress inet = null;
+//                    try {
+//                        inet = InetAddress.getLocalHost();
+//                    } catch (UnknownHostException e) {
+//                        e.printStackTrace();
+//                    }
+//                    ipAddress = inet.getHostAddress();
+//                }
+//            }
+//            // 对于通过多个代理的情况，第一个IP为客户端真实IP,多个IP按照','分割
+//            if (ipAddress != null && ipAddress.length() > 15) {
+//                // = 15
+//                if (ipAddress.indexOf(",") > 0) {
+//                    ipAddress = ipAddress.substring(0, ipAddress.indexOf(","));
+//                }
+//            }
+//        } catch (Exception e) {
+//            ipAddress = "";
+//        }
+//        return ipAddress;
+//    }
 
     /**
      * 解析ip地址

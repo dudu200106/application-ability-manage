c6c8455e1ea82bff3184ad0d39d918228b7ea68f
SeanDu <1247187273@qq.com>
2024-02-05 17:45:56 +0800

    fix: 修复3

--- src/main/java/com/dsj/csp/manage/service/impl/AbilityApiServiceImpl.java
+++ src/main/java/com/dsj/csp/manage/service/impl/AbilityApiServiceImpl.java
@@ -2,6 +2,7 @@ package com.dsj.csp.manage.service.impl;
 
 
 import cn.hutool.core.date.DateTime;
+import cn.hutool.core.util.ObjectUtil;
 import com.baomidou.mybatisplus.core.conditions.update.LambdaUpdateWrapper;
 import com.baomidou.mybatisplus.core.toolkit.Wrappers;
 import com.baomidou.mybatisplus.extension.service.impl.ServiceImpl;
@@ -17,6 +18,7 @@ import org.springframework.transaction.annotation.Propagation;
 import org.springframework.transaction.annotation.Transactional;
 
 import java.util.Date;
+import java.util.List;
 
 
 @Transactional(propagation = Propagation.REQUIRED)
@@ -55,5 +57,16 @@ public class AbilityApiServiceImpl extends ServiceImpl<AbilityApiMapper, Ability
                                                 "接口下线完毕!";
         return auditMsg;
 
+    }
+
+    @Override
+    public List<Long> getApiIds(String keyword) {
+        List<AbilityApiEntity> apis = this.list(Wrappers.lambdaQuery(AbilityApiEntity.class)
+                .and(ObjectUtil.isEmpty(keyword), i->i.like(AbilityApiEntity::getApiName, keyword)
+                        .or().like(AbilityApiEntity::getApiDesc, keyword)
+                        .or().like(AbilityApiEntity::getApiUrl, keyword))
+        );
+        List<Long> ids = apis.stream().map(e->e.getApiId()).toList();
+        return ids;
     }
 }

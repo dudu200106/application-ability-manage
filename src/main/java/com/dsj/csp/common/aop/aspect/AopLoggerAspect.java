73a7ef5950b890c35951363b408474a0d5a80eaa
dushunchang <shunchangdu@sina.com>
2024-02-07 13:12:48 +0800

    修复日志切面

--- src/main/java/com/dsj/csp/common/aop/aspect/AopLoggerAspect.java
+++ src/main/java/com/dsj/csp/common/aop/aspect/AopLoggerAspect.java
@@ -10,6 +10,7 @@ import org.aspectj.lang.JoinPoint;
 import org.aspectj.lang.ProceedingJoinPoint;
 import org.aspectj.lang.annotation.Around;
 import org.aspectj.lang.annotation.Aspect;
+import org.aspectj.lang.annotation.Before;
 import org.aspectj.lang.annotation.Pointcut;
 import org.aspectj.lang.reflect.MethodSignature;
 import org.slf4j.Logger;
@@ -49,8 +50,8 @@ public class AopLoggerAspect {
      * @param point
      * @return
      */
-    @Around("aopLoggerAspect()")
-    public Object doAround(ProceedingJoinPoint point) {
+    @Before("aopLoggerAspect()")
+    public Object doAround(JoinPoint point) {
         LogEntity logEntity = new LogEntity();
         RequestAttributes requestAttributes = RequestContextHolder.getRequestAttributes();
         ServletRequestAttributes servletRequestAttributes = (ServletRequestAttributes) requestAttributes;
@@ -58,7 +59,9 @@ public class AopLoggerAspect {
         Object result = null;
         long startTime = System.currentTimeMillis();
         try {
-            result = point.proceed();
+            result=point.getSignature();
+            System.out.println(result);
+//            result = point.proceed();
         } catch (Throwable throwable) {
             throwable.printStackTrace();
             logger.error(throwable.getMessage());

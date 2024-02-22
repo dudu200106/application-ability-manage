a520c1d89175cff1c6f1ee94f599972194a92878
SeanDu <1247187273@qq.com>
2024-02-22 15:43:11 +0800

    fix: 修改过滤器配置的位置

--- src/main/java/com/dsj/csp/common/config/web/WebMvcConfig.java
+++ src/main/java/com/dsj/csp/common/config/web/WebMvcConfig.java
@@ -1,11 +1,17 @@
 package com.dsj.csp.common.config.web;
 
+import com.dsj.csp.common.aop.aspect.LoginUserHandlerMethodArgumentResolver;
+import com.dsj.csp.common.filter.TokenValidationFilter;
+import jakarta.servlet.Filter;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.web.servlet.FilterRegistrationBean;
 import org.springframework.context.ApplicationContext;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.method.support.HandlerMethodArgumentResolver;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
+import java.util.Arrays;
 import java.util.List;
 import java.util.Map;
 
@@ -18,12 +24,28 @@ import java.util.Map;
 @Configuration
 public class WebMvcConfig implements WebMvcConfigurer {
     @Autowired
-    private ApplicationContext context;
+    private ApplicationContext applicationContext;
 
     @Override
     public void addArgumentResolvers(List<HandlerMethodArgumentResolver> resolvers) {
-        Map<String, HandlerMethodArgumentResolver> beans = context.getBeansOfType(HandlerMethodArgumentResolver.class);
-        beans.forEach((name, bean) -> resolvers.add(bean));
+        LoginUserHandlerMethodArgumentResolver resolver = new LoginUserHandlerMethodArgumentResolver();
+        applicationContext.getAutowireCapableBeanFactory().autowireBean(resolver);
+        resolvers.add(resolver);
+    }
+
+    /*注册过滤器*/
+    @Bean
+    public FilterRegistrationBean<Filter> orderFilter() {
+        // 利用工厂给容器外的对象注入所需组件
+        TokenValidationFilter filterBean = new TokenValidationFilter();
+        applicationContext.getAutowireCapableBeanFactory().autowireBean(filterBean);
+
+        FilterRegistrationBean<Filter> filter = new FilterRegistrationBean<>();
+        filter.setName("TokenValidationFilter");
+        filter.setUrlPatterns(Arrays.asList("/doc/*"));
+        filter.setFilter(filterBean);//指定优先级
+        filter.setOrder(-1);
+        return filter;
     }
 
 }

cf34cc7f68ba993af32dafa98e15ca5eec47e0a2
SeanDu <1247187273@qq.com>
2024-02-22 10:40:48 +0800

    fix: 修改token过滤器

--- src/main/java/com/dsj/csp/common/config/web/FilterConfig.java
+++ src/main/java/com/dsj/csp/common/config/web/FilterConfig.java
@@ -7,11 +7,8 @@ import org.springframework.boot.web.servlet.FilterRegistrationBean;
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.web.method.support.HandlerMethodArgumentResolver;
 
 import java.util.Arrays;
-import java.util.List;
-import java.util.Map;
 
 /**
  * @author SeanDu
@@ -23,22 +20,22 @@ public class FilterConfig {
     @Autowired
     private ApplicationContext applicationContext;
 
-    @Bean
-    public TokenValidationFilter getTokenValidationFilter() {
-        // 将未纳入容器的Filter Bean纳入容器
-        return new TokenValidationFilter();
-    }
+//    @Bean
+//    public TokenValidationFilter getTokenValidationFilter() {
+//        // 将未纳入容器的Filter Bean纳入容器
+//        return new TokenValidationFilter();
+//    }
 
     @Bean
     public FilterRegistrationBean<Filter> orderFilter() {
         // 利用工厂给容器外的对象注入所需组件
-//        TokenValidationFilter filterBean = new TokenValidationFilter();
-//        applicationContext.getAutowireCapableBeanFactory().autowireBean(filterBean);
+        TokenValidationFilter filterBean = new TokenValidationFilter();
+        applicationContext.getAutowireCapableBeanFactory().autowireBean(filterBean);
 
         FilterRegistrationBean<Filter> filter = new FilterRegistrationBean<>();
         filter.setName("TokenValidationFilter");
         filter.setUrlPatterns(Arrays.asList("/doc/*"));
-        filter.setFilter(getTokenValidationFilter());//指定优先级
+        filter.setFilter(filterBean);//指定优先级
         filter.setOrder(-1);
         return filter;
     }
--- src/main/java/com/dsj/csp/common/filter/TokenValidationFilter.java
+++ src/main/java/com/dsj/csp/common/filter/TokenValidationFilter.java
@@ -1,14 +1,13 @@
 package com.dsj.csp.common.filter;
 
-
 import com.dsj.common.dto.Result;
 import com.dsj.csp.common.exception.FlowException;
 import com.dsj.csp.manage.service.UserApproveService;
 import com.fasterxml.jackson.databind.ObjectMapper;
-import jakarta.annotation.Resource;
 
 import jakarta.servlet.*;
 import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
 import org.springframework.beans.factory.annotation.Autowired;
 
 import java.io.IOException;
@@ -30,22 +29,22 @@ public class TokenValidationFilter implements Filter {
 
     @Override
     public void doFilter(ServletRequest servletRequest, ServletResponse servletResponse, FilterChain filterChain) throws IOException, ServletException {
-        //在这里进行Token验证的逻辑
         //检查请求中的Token是否有效，是否包含正确的权限等
         //如果Token验证通过，继续请求链的执行
         //如果Token验证失败，可以返回适当的错误响应或进行重定向
-        //例如，假设您在请求头中传递了一个名为"Authorization"的Token
         HttpServletRequest httpServletRequest = (HttpServletRequest) servletRequest;
         String token = httpServletRequest.getHeader( "Authorization");
         try{
             // Token验证通过，继续请求链的执行
             userApproveService.identify(token);
             filterChain.doFilter(servletRequest, servletResponse);
-        }catch (FlowException e){
-            //Token验证失败，返回错误响应或重定向到登录页面
-            Result result = Result.failed(e.getCode(), e.getMessage());
-            servletResponse.setContentType("application/json");
-            servletResponse.getWriter().write(new ObjectMapper().writeValueAsString(result));
+        }catch (Exception e){
+            // Token验证失败，返回错误响应或重定向到登录页面
+            HttpServletResponse httpServletResponse = (HttpServletResponse) servletResponse;
+            Result result = Result.failed(-1, e.getMessage());
+            httpServletResponse.setContentType("application/json;charset=UTF-8");
+            httpServletResponse.getWriter().write(new ObjectMapper().writeValueAsString(result));
+//            httpServletResponse.sendRedirect("/home");
         }
     }
 

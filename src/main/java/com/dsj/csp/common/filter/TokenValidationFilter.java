a9e65295bd74a4da7f053a45912bcbc0dfebde46
SeanDu <1247187273@qq.com>
2024-02-23 14:05:35 +0800

    feat：修改token验证过滤器,暂不使用

--- src/main/java/com/dsj/csp/common/filter/TokenValidationFilter.java
+++ src/main/java/com/dsj/csp/common/filter/TokenValidationFilter.java
@@ -1,5 +1,6 @@
 package com.dsj.csp.common.filter;
 
+import com.baomidou.mybatisplus.core.toolkit.StringUtils;
 import com.dsj.common.dto.Result;
 import com.dsj.csp.common.exception.FlowException;
 import com.dsj.csp.manage.service.UserApproveService;
@@ -30,20 +31,23 @@ public class TokenValidationFilter implements Filter {
     @Override
     public void doFilter(ServletRequest servletRequest, ServletResponse servletResponse, FilterChain filterChain) throws IOException, ServletException {
         //检查请求中的Token是否有效，是否包含正确的权限等
-        //如果Token验证通过，继续请求链的执行
-        //如果Token验证失败，可以返回适当的错误响应或进行重定向
+        //如果Token验证通过，继续请求链的执行; 验证失败，可以返回适当的错误响应或进行重定向
         HttpServletRequest httpServletRequest = (HttpServletRequest) servletRequest;
-        String token = httpServletRequest.getHeader( "Authorization");
+        String token = httpServletRequest.getHeader( "Accesstoken");
+        if (StringUtils.isEmpty(token)){
+            token = httpServletRequest.getHeader( "Authorization");
+        }
         try{
             // Token验证通过，继续请求链的执行
             userApproveService.identify(token);
             filterChain.doFilter(servletRequest, servletResponse);
-        }catch (Exception e){
+        }catch (FlowException e){
             // Token验证失败，返回错误响应或重定向到登录页面
             HttpServletResponse httpServletResponse = (HttpServletResponse) servletResponse;
             Result result = Result.failed(-1, e.getMessage());
             httpServletResponse.setContentType("application/json;charset=UTF-8");
             httpServletResponse.getWriter().write(new ObjectMapper().writeValueAsString(result));
+//            // 重定向回首页
 //            httpServletResponse.sendRedirect("/home");
         }
     }
